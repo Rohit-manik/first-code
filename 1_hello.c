31aebee700ff14a0dcbd3282ef3dc4d335f2d836
Rohit-manik <rohitmanik77@gmail.com>
2026-09-06 21:28:15 +0530

    1_hello.c

--- 1_hello.c
+++ 1_hello.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
-void main(){
-   printf("Hello\n");
+#include <stdio.h>
+void main()
+{
+   printf("Hello everyone\n");
    printf("My name is Rohit Manik\n");
+   printf("A B.Tech student at Jis University\n");
+   printf("CSE with specialization in (AI/ML)");
 }
