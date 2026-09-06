a10df5b454f872cd5edf70ef2f648ee6e66537ba
Rohit-manik <rohitmanik77@gmail.com>
2026-09-06 21:46:02 +0530

    Sum and Difference two floating number

---
+++ 4_Sum_and_Difference_two_floating_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+   float a, b, c, d;
+   printf("Enter 2 floating numbers\n");
+   scanf("%f,%f", &a, &b);
+   c = a + b;
+   d = a - b;
+   printf("sum = %f\n", c);
+   printf("sub = %f\n", d);
+}
